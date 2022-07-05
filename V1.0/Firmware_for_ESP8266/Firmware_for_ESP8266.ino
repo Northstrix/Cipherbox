3393b1e599002cc288f3ff0a3ca2ebf8f643915e
Maxim Bortnikov <43994704+Northstrix@users.noreply.github.com>
2022-07-05 21:54:32 +0700

    Add files via upload

---
+++ V1.0/Firmware_for_ESP8266/Firmware_for_ESP8266.ino
@@ -0,0 +1,415 @@
+/*
+Cipherbox
+Distributed under the MIT License
+© Copyright Maxim Bortnikov 2022
+For more information please visit
+https://github.com/Northstrix/Cipherbox
+Required libraries:
+https://github.com/zhouyangchao/AES
+https://github.com/peterferrie/serpent
+https://github.com/ddokkaebi/Blowfish
+https://github.com/ulwanski/sha512
+https://github.com/adafruit/Adafruit-GFX-Library
+https://github.com/adafruit/Adafruit_ILI9341
+https://github.com/adafruit/Adafruit_BusIO
+https://github.com/GyverLibs/GyverBus
+https://github.com/PaulStoffregen/PS2Keyboard
+https://github.com/siara-cc/esp32_arduino_sqlite3_lib
+https://github.com/miguelbalboa/rfid
+https://github.com/platisd/nokia-5110-lcd-library
+*/
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#include "sha512.h"
+#include "aes.h"
+#include "serpent.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+Adafruit_PCD8544 display = Adafruit_PCD8544(D4, D3, D2, D1, D0);
+typedef struct struct_message {
+  char l_srp[16];
+  char r_srp[16];
+  bool n;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+String plt;
+byte tmp_st[8];
+int tmp_s[8];
+int m = 2; // AES-256
+
+uint8_t projection_key[32] = {
+0x5e,0x74,0x6a,0x0e,
+0xe8,0x8f,0x34,0x5e,
+0xb5,0x1b,0x1e,0xc5,
+0x56,0x4b,0x0c,0x11,
+0x46,0xc4,0x71,0xdd,
+0xc4,0x40,0xee,0xb8,
+0x5a,0x8b,0xf7,0xd3,
+0x7d,0xc2,0x6e,0x38
+};
+uint8_t proj_serp_key[32] = {
+0x18,0xed,0x46,0x1f,
+0xab,0xfc,0x6d,0xda,
+0x0f,0xa9,0xc8,0xfb,
+0x96,0x3e,0x9a,0xb5,
+0x8b,0xb2,0xbe,0x98,
+0x54,0xd8,0x8a,0x2b,
+0x1d,0xb2,0x8e,0x57,
+0xd9,0xb3,0x5a,0x42
+};
+
+void incr_projection_key() {
+  if (projection_key[0] == 255) {
+    projection_key[0] = 0;
+    if (projection_key[1] == 255) {
+      projection_key[1] = 0;
+      if (projection_key[2] == 255) {
+        projection_key[2] = 0;
+        if (projection_key[3] == 255) {
+          projection_key[3] = 0;
+
+          if (projection_key[4] == 255) {
+            projection_key[4] = 0;
+            if (projection_key[5] == 255) {
+              projection_key[5] = 0;
+              if (projection_key[6] == 255) {
+                projection_key[6] = 0;
+                if (projection_key[7] == 255) {
+                  projection_key[7] = 0;
+
+                  if (projection_key[8] == 255) {
+                    projection_key[8] = 0;
+                    if (projection_key[9] == 255) {
+                      projection_key[9] = 0;
+                      if (projection_key[10] == 255) {
+                        projection_key[10] = 0;
+                        if (projection_key[11] == 255) {
+                          projection_key[11] = 0;
+
+                          if (projection_key[12] == 255) {
+                            projection_key[12] = 0;
+                            if (projection_key[13] == 255) {
+                              projection_key[13] = 0;
+                              if (projection_key[14] == 255) {
+                                projection_key[14] = 0;
+                                if (projection_key[15] == 255) {
+                                  projection_key[15] = 0;
+                                } else {
+                                  projection_key[15]++;
+                                }
+                              } else {
+                                projection_key[14]++;
+                              }
+                            } else {
+                              projection_key[13]++;
+                            }
+                          } else {
+                            projection_key[12]++;
+                          }
+
+                        } else {
+                          projection_key[11]++;
+                        }
+                      } else {
+                        projection_key[10]++;
+                      }
+                    } else {
+                      projection_key[9]++;
+                    }
+                  } else {
+                    projection_key[8]++;
+                  }
+
+                } else {
+                  projection_key[7]++;
+                }
+              } else {
+                projection_key[6]++;
+              }
+            } else {
+              projection_key[5]++;
+            }
+          } else {
+            projection_key[4]++;
+          }
+
+        } else {
+          projection_key[3]++;
+        }
+      } else {
+        projection_key[2]++;
+      }
+    } else {
+      projection_key[1]++;
+    }
+  } else {
+    projection_key[0]++;
+  }
+}
+
+void incr_proj_serp_key() {
+  if (proj_serp_key[15] == 255) {
+    proj_serp_key[15] = 0;
+    if (proj_serp_key[14] == 255) {
+      proj_serp_key[14] = 0;
+      if (proj_serp_key[13] == 255) {
+        proj_serp_key[13] = 0;
+        if (proj_serp_key[12] == 255) {
+          proj_serp_key[12] = 0;
+
+          if (proj_serp_key[11] == 255) {
+            proj_serp_key[11] = 0;
+            if (proj_serp_key[10] == 255) {
+              proj_serp_key[10] = 0;
+              if (proj_serp_key[9] == 255) {
+                proj_serp_key[9] = 0;
+                if (proj_serp_key[8] == 255) {
+                  proj_serp_key[8] = 0;
+
+                  if (proj_serp_key[7] == 255) {
+                    proj_serp_key[7] = 0;
+                    if (proj_serp_key[6] == 255) {
+                      proj_serp_key[6] = 0;
+                      if (proj_serp_key[5] == 255) {
+                        proj_serp_key[5] = 0;
+                        if (proj_serp_key[4] == 255) {
+                          proj_serp_key[4] = 0;
+
+                          if (proj_serp_key[3] == 255) {
+                            proj_serp_key[3] = 0;
+                            if (proj_serp_key[2] == 255) {
+                              proj_serp_key[2] = 0;
+                              if (proj_serp_key[1] == 255) {
+                                proj_serp_key[1] = 0;
+                                if (proj_serp_key[0] == 255) {
+                                  proj_serp_key[0] = 0;
+                                } else {
+                                  proj_serp_key[0]++;
+                                }
+                              } else {
+                                proj_serp_key[1]++;
+                              }
+                            } else {
+                              proj_serp_key[2]++;
+                            }
+                          } else {
+                            proj_serp_key[3]++;
+                          }
+
+                        } else {
+                          proj_serp_key[4]++;
+                        }
+                      } else {
+                        proj_serp_key[5]++;
+                      }
+                    } else {
+                      proj_serp_key[6]++;
+                    }
+                  } else {
+                    proj_serp_key[7]++;
+                  }
+
+                } else {
+                  proj_serp_key[8]++;
+                }
+              } else {
+                proj_serp_key[9]++;
+              }
+            } else {
+              proj_serp_key[10]++;
+            }
+          } else {
+            proj_serp_key[11]++;
+          }
+
+        } else {
+          proj_serp_key[12]++;
+        }
+      } else {
+        proj_serp_key[13]++;
+      }
+    } else {
+      proj_serp_key[14]++;
+    }
+  } else {
+    proj_serp_key[15]++;
+  }
+}
+
+int getNum(char ch)
+{
+    int num=0;
+    if(ch>='0' && ch<='9')
+    {
+        num=ch-0x30;
+    }
+    else
+    {
+        switch(ch)
+        {
+            case 'A': case 'a': num=10; break;
+            case 'B': case 'b': num=11; break;
+            case 'C': case 'c': num=12; break;
+            case 'D': case 'd': num=13; break;
+            case 'E': case 'e': num=14; break;
+            case 'F': case 'f': num=15; break;
+            default: num=0;
+        }
+    }
+    return num;
+}
+
+void print(const char *msg, const uint8_t *buf)
+{
+  Serial.printf("%s", msg);
+  int i;
+  for(i = 0; i < 16; ++i)
+    Serial.printf("%02x ", buf[i]);
+  Serial.printf("\n");
+}
+
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  if (myData.n == false){
+    plt = "";
+    display.clearDisplay();
+  }
+  delayMicroseconds(24);
+  decr_Serpent(myData.l_srp, false);
+  decr_Serpent(myData.r_srp, true);
+  incr_projection_key();
+  incr_proj_serp_key();
+  incr_proj_serp_key();
+}
+
+void decr_Serpent(char res[], bool pass){
+      uint8_t ct1[32], pt1[32], key[64];
+      int plen, clen, i, j;
+      serpent_key skey;
+      serpent_blk ct2;
+      uint32_t *p;
+  
+  for (i=0; i<1; i++) {
+    hex2binproj (key);
+  
+    // set key
+    memset (&skey, 0, sizeof (skey));
+    p=(uint32_t*)&skey.x[0][0];
+    
+    serpent_setkey (&skey, key);
+    //Serial.printf ("\nkey=");
+
+    for (j=0; j<sizeof(skey)/sizeof(serpent_subkey_t)*4; j++) {
+      if ((j % 8)==0) putchar('\n');
+      //Serial.printf ("%08X ", p[j]);
+    }
+
+    for(int i = 0; i <16; i++)
+      ct2.b[i] = res[i];
+    /*
+    Serial.printf ("\n\n");
+    for(int i = 0; i<16; i++){
+    Serial.printf("%x", ct2.b[i]);
+    Serial.printf(" ");
+    */
+    }
+    //Serial.printf("\n");
+    serpent_encrypt (ct2.b, &skey, SERPENT_DECRYPT);
+    /*
+    for (int i=0; i<16; i++) {
+      Serial.print(int(ct2.b[i]));
+      Serial.print(" ");
+    }
+    Serial.println();
+    */
+    if (pass == false){
+      for (int i = 0; i<8; i++){
+        tmp_s[i] = ct2.b[i];
+      }
+    }
+    if (pass == true){
+      int t_dec[16];
+      for (int i = 0; i<8; i++){
+        t_dec[i] = tmp_s[i];
+      }
+      for (int i = 0; i<8; i++){
+        t_dec[i+8] = ct2.b[i];
+      }
+      decr_AES(t_dec);
+    }
+}
+
+void decr_AES(int res[]){
+      uint8_t ret_text[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      uint8_t cipher_text[16] = {0};
+      for(int i = 0; i<16; i++){
+        int c = int(res[i]);
+        cipher_text[i] = c;
+      }
+      uint32_t projection_key_bit[3] = {128, 192, 256};
+      int i = 0;
+      aes_context ctx;
+      aes_set_key(&ctx, projection_key, projection_key_bit[m]);
+      aes_decrypt_block(&ctx, ret_text, cipher_text);
+      for (i = 0; i < 8; ++i) {
+        //Serial.print(char(ret_text[i]));
+        //Serial.println(ret_text[i]);
+        if (ret_text[i] != 0){
+          plt += char(ret_text[i]);
+        }
+      }
+      Serial.print("Received text:");
+      Serial.println(plt);
+      display.setTextSize(1);
+      display.setTextColor(BLACK);
+      display.setCursor(0,0);
+      display.println(plt);
+      display.display(); 
+}
+
+size_t hex2binproj (void *bin) {
+  size_t len, i;
+  int x;
+  uint8_t *p=(uint8_t*)bin;
+  for (i=0; i < 32; i++) {
+    p[i] = (uint8_t)proj_serp_key[i];
+  }
+  return 32;
+}
+
+void cbox(){
+  Serial.println("\nCipherbox");
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+  display.println("Cipherbox");
+  display.display();
+}
+ 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  display.begin();
+  display.setContrast(60);
+  display.clearDisplay();
+  display.display();
+  cbox();
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+
+}
